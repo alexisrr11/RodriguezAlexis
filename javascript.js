83c73f7559b562b931891469d2714bb4cb7b9347
alexisrr11 <alexis.r4995@gmail.com>
2024-12-19 18:43:57 -0300

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -9,3 +9,16 @@ abrir.addEventListener("click", () => {
 cerrar.addEventListener("click", () => {
     menu.classList.remove("visible");
 })
+
+const $form = document.querySelector('#form')
+const $buttonMailto = document.querySelector('#mailto')
+
+
+$form.addEventListener('submit', handleSubtmit)
+
+function handleSubtmit (event){
+    event.preventDefault()
+    const form = new FormData(this)
+    $buttonMailto.setAttribute('href', `mailto:alexis.r4995@gmail.com?subject=${form.get('name')}${form.get('email')}&body=${form.get('mensaje')}`)
+    $buttonMailto.click()
+}
