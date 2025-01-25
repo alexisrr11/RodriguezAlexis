00e616382c01af7663495c43d8bcaf933cd4fda6
alexisrr11 <alexis.r4995@gmail.com>
2025-01-24 23:58:29 -0300

    Update descProyecto.js

--- descProyecto.js
+++ descProyecto.js
@@ -1,27 +1,29 @@
-let hideText_btn = document.getElementById("hideText_btn");
+function toggleText(buttonId, contentId, className) {
+    let button = document.getElementById(buttonId);
+    let content = document.getElementById(contentId);
 
-let hideText = document.getElementById("hideText");
+    button.addEventListener("click", () => {
+        content.classList.toggle(className);
+    });
 
-hideText_btn.addEventListener("click", ToggleText);
-
-function ToggleText() {
-    hideText.classList.toggle("show");
+    content.addEventListener("click", () => {
+        content.classList.remove(className);
+    });
 }
 
-hideText.addEventListener("click", () => {
-    hideText.classList.remove("show");
-})
+toggleText("hideText_btn", "hideText", "show");
+toggleText("hideText_btn1", "hideText1", "show1");
+toggleText("hideText_btn2", "hideText2", "show2");
 
-let hideText_btn1 = document.getElementById("hideText_btn1");
 
-let hideText1 = document.getElementById("hideText1");
-
-hideText_btn1.addEventListener("click", ToggleTxt);
-
-function ToggleTxt() {
-    hideText1.classList.toggle("show1");
-}
-
-hideText1.addEventListener("click", () => {
-    hideText1.classList.remove("show1");
-})
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener("click", function (e) {
+        e.preventDefault();
+  
+        const target = document.querySelector(this.getAttribute("href"));
+        target.scrollIntoView({
+            behavior: "smooth",
+            block: "start"
+        });
+    });
+  });
